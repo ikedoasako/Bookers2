cf8876f9157b225585d14398f74bb61da6f20d36
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-03-25 12:47:47 +0000

    [2]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  root to:"homes#top"
+  get "homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :destroy]
+  #get 'users/show'
+  #get 'users/edit'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: => 'homes#top'
+
 
 end
