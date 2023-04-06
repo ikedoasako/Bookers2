fa9333d0af1e2dfc813f3cd1432a00ec3d7d0cb9
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-04-06 14:40:49 +0000

    [18]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -28,5 +29,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :get_profile_image, :introduction)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path) unless @user =- current_user
   end
 end
