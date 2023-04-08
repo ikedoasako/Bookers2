8e16a388fea94cfbc4a298e7515efabee9aa6fb1
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-04-08 12:51:52 +0000

    [22]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :get_profile_image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
-  get "/homes/about" => "homes#about", as: "about"
+  get "/home/about" => "homes#about", as: "about"
   patch 'users/:id' => 'users#update', as: 'update_user'
   resources :users
   resources :books
